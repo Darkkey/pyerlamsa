b1377f2eedb7fb659d5b6acfa4dfc58e7bea7f8c
Darkkey <alexanderdarkkey@gmail.com>
2020-11-20 19:27:57 +0200

    updated version info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyerlamsa',
-      version='0.1',
+      version='0.1.1',
       description='Python client for erlamsa fuzzer',
       url='http://github.com/Darkkey/pyerlamsa',
       author='Alexander Bolshev',
